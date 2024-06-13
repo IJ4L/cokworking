bf7a70d139ba275f8b5a8328f5bd03fe6ae6cbbd
ijull <rijal9246@gmail.com>
2024-06-13 21:22:44 +0800

    chore: initial commit

---
+++ resources/views/landing/hero.blade.php
@@ -0,0 +1,59 @@
+{{-- Hero --}}
+<div class="bg-cream">
+    <div class="max-w-screen-xl px-8 mx-auto flex flex-col lg:pt-28 lg:pb-28 lg:flex-row items-start">
+        <div class="flex flex-col w-full lg:w-6/12 justify-center  items-start text-center lg:text-left mb-5 md:mb-0">
+            <h1 data-aos="fade-right" data-aos-once="true" class="my-4 text-5xl font-bold leading-tight text-[#0C1844]">
+                <span class="text-[#C80036]">Welcome</span> Connect, Collaborate, Innovate
+            </h1>
+            <p data-aos="fade-down" data-aos-once="true" data-aos-delay="300" class="leading-normal text-2xl mb-8">
+                A hub for leaders,
+                communities and entrepreneurs to collaborate
+                and make positive impact focused on inspiring a
+                nutritious life through health, education and green pillars.</p>
+            <div data-aos="fade-up" data-aos-once="true" data-aos-delay="700"
+                class="w-full md:flex items-center justify-center lg:justify-start md:space-x-5">
+                <button
+                    class="lg:mx-0 bg-yellow-500 text-white text-xl font-bold rounded-full py-4 px-9 focus:outline-none transform transition hover:scale-110 duration-300 ease-in-out">
+                    Get in touch
+                </button>
+                <div
+                    class="flex items-center justify-center space-x-3 mt-5 md:mt-0 focus:outline-none transform transition hover:scale-110 duration-300 ease-in-out">
+                    <button class="bg-white w-14 h-14 rounded-full flex items-center justify-center">
+                        <svg class="w-7 h-7" viewBox="0 0 24 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M13 13V18.585L14.8284 16.7574L16.2426 18.1716L12 22.4142L7.75736 18.1716L9.17157 16.7574L11 18.585V13H13ZM12 2C15.5934 2 18.5544 4.70761 18.9541 8.19395C21.2858 8.83154 23 10.9656 23 13.5C23 16.3688 20.8036 18.7246 18.0006 18.9776L18.0009 16.9644C19.6966 16.7214 21 15.2629 21 13.5C21 11.567 19.433 10 17.5 10C17.2912 10 17.0867 10.0183 16.8887 10.054C16.9616 9.7142 17 9.36158 17 9C17 6.23858 14.7614 4 12 4C9.23858 4 7 6.23858 7 9C7 9.36158 7.03838 9.7142 7.11205 10.0533C6.91331 10.0183 6.70879 10 6.5 10C4.567 10 3 11.567 3 13.5C3 15.2003 4.21241 16.6174 5.81986 16.934L6.00005 16.9646L6.00039 18.9776C3.19696 18.7252 1 16.3692 1 13.5C1 10.9656 2.71424 8.83154 5.04648 8.19411C5.44561 4.70761 8.40661 2 12 2Z"
+                                fill="#C80036">
+                            </path>
+                        </svg>
+                    </button>
+                    <span class="cursor-pointer">Unduh company profile</span>
+                </div>
+            </div>
+        </div>
+        <div class="w-full lg:w-6/12 lg:-mt-12 relative" id="hero">
+            <img data-aos="fade-up" data-aos-once="true"
+                class="w-96 h-96 mx-auto 2xl:-mb-20 md:mt-16 md:mb-28 object-cover rounded-3xl md:rounded-full"
+                src="{{ asset('images/hero.jpg') }}" />
+        </div>
+    </div>
+    <div class="text-white -mt-14 sm:-mt-24 lg:-mt-36 z-40 relative">
+        <svg class="xl:h-40 xl:w-full" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120"
+            preserveAspectRatio="none">
+            <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z"
+                fill="currentColor"></path>
+        </svg>
+        <div class="bg-white w-full h-10 -mt-px"></div>
+    </div>
+</div>
+<div class="max-w-4xl mx-auto">
+    <h1 class="text-center mb-6 text-gray-400 font-medium">Trusted by 5,000+ Companies Worldwide</h1>
+    <div class="grid grid-cols-3 lg:grid-cols-6 gap-4 justify-items-center">
+        <img class="h-7" src="{{ asset('images/company/google.svg') }}">
+        <img class="h-7" src="{{ asset('images/company/netflix.svg') }}">
+        <img class="h-7" src="{{ asset('images/company/airbnb.svg') }}">
+        <img class="h-7 transform translate-y-2" src="{{ asset('images/company/amazon.svg') }}">
+        <img class="h-7" src="{{ asset('images/company/facebook.svg') }}">
+        <img class="h-7" src="{{ asset('images/company/grab.svg') }}">
+    </div>
+</div>
+{{-- Hero --}}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="favicon.ico">
+    <!-- Alpine -->
+    <script type="module" src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"></script>
+    <script nomodule src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine-ie11.min.js" defer></script>
+    <!-- AOS -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <!-- Poppins font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+
+    @include('partials.nav')
+
+    @include('landing.hero')
+
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+
+</body>
+
+</html>
