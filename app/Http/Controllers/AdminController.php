27397e35497d63f7431e324ea1b012d2448f20ea
ijull <rijal9246@gmail.com>
2024-11-22 05:04:13 +0800

    fix: ui and functional

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,7 +20,7 @@ class AdminController extends Controller
                 $query->where('name', 'Komunitas');
             })->count(),
             'Bisnis'           => Reservation::whereHas('organization', function ($query) {
-                $query->where('name', 'Bisnis');
+                $query->where('name', 'Bisnis / UMKM');
             })->count(),
             'Instansi'         => Reservation::whereHas('organization', function ($query) {
                 $query->where('name', 'Instansi');
@@ -29,7 +29,7 @@ class AdminController extends Controller
                 $query->where('name', 'Media');
             })->count(),
             'Other'            => Reservation::whereHas('organization', function ($query) {
-                $query->whereNotIn('name', ['Komunitas', 'Perguruan Tinggi']);
+                $query->whereNotIn('name', ['Komunitas', 'Perguruan Tinggi', 'Bisnis / UMKM', 'Instansi', 'Media']);
             })->count(),
         ];
 
@@ -37,7 +37,7 @@ class AdminController extends Controller
 
         $organizations = Organization::all();
 
-        $reservations = Reservation::with(['room', 'organization'])->orderBy('id', 'desc')->paginate(10);
+        $reservations = Reservation::with(['room', 'organization'])->paginate(10);
 
         if ($request->has('organization_id') && $request->organization_id != '') {
             $query->where('organization_id', $request->organization_id);
